3b65003c7d410aabd2f0d3c089abc084474f6a7b
SaikiranTamide <tamidesaikiran912@gmail.com>
2024-07-31 15:45:29 +0530

    First commit

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/submit', methods=['POST'])
+@app.route('/', methods=['POST'])
 def submit():
     # Get the JSON data from the request
     data = request.get_json()
